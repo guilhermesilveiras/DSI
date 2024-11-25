43caaf6fb1b6077884a90566560396442d853b04
Daniel Santana <135668123+DanielSantana47@users.noreply.github.com>
2024-11-25 16:48:56 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return(
+        <View className="h-full w-full justify-center items-center">
+            <Text>Home</Text>
+        </View>
+    )
+}
