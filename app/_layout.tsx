83177fc24448561475eafce01007ac4160da17ff
Hivison Santos <84513910+FuscaRetratil@users.noreply.github.com>
2024-12-09 16:34:10 -0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
 import '../global.css'
+import { ScreenStack } from "react-native-screens";
 
 export default function Layout() {
     return(
         <Stack screenOptions={{headerShown: false}}>
+            <Stack.Screen name="prelogin"/>
             <Stack.Screen name="index"/>
             <Stack.Screen name="cadastro"/>
             <Stack.Screen name="forgot-password"/>
