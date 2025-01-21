87a6e539cde67085c292b5c517dfaa9b458e0391
Hivison Santos <84513910+FuscaRetratil@users.noreply.github.com>
2025-01-21 01:38:18 -0300

    Adiciona navegação da tela de perfil

--- components/main/header.tsx
+++ components/main/header.tsx
@@ -6,13 +6,17 @@ export const Header = ()=> {
     const handleListButton = ()=> {
         router.navigate('/wish-list')
     }
+
+    const handleProfileButton = ()=> {
+        router.navigate('/profile')
+    }
     
     return(
         <View className="w-full flex-row items-center justify-between px-10 py-8">
             <Pressable onPress={handleListButton}>
                 <Icon name="list-ul" size={24} color={"#002932"}/>
             </Pressable>
-            <Pressable>
+            <Pressable onPress={handleProfileButton}>
                 <Icon name="user-circle" size={24} color={"#002932"} />
             </Pressable>
         </View>
